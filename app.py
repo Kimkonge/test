3de598e53abc72e55259407b2ba88bb73707f842
Kimkonge <valentinkim1@mail.ru>
2025-08-11 20:24:54 +0300

    initial commit

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 print("Hello world")
-git status - проверка статуса
-git add <название файла> - добавление в index
-git commit -m "комментарий к комиту"
-git log - отобразить комиты
+# git status - проверка статуса
+# git add <название файла> - добавление в index
+# git commit -m "комментарий к комиту"
+# git log - отобразить комиты
